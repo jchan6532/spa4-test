4620eee0be347aa7c5eb729d01aea030878502c1
dremm4549 <mDremo5093@conestogac.on.ca>
2022-03-18 12:21:24 -0400

    file skeleton added

---
+++ CHAT-SYSTEM/chat-client/src/chat-client.c
@@ -0,0 +1,13 @@
+/* FILE :           chat-client.c
+ * PROJECT :        SENG2030 - Assignment #4
+ * PROGRAMMER :    	Justin Chan, Michael Dremo, Paige Lam & Erica Luksts
+ * FIRST VERSION :  2022-03-18
+ * DESCRIPTION :    Temp description
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+
+}
